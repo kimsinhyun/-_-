ef7e8ff9357ab1d137d29c315b1afe9259c60fc8
sinhyun Kim <ksh415622@naver.com>
2024-09-16 16:35:10 +0900

    wip

---
+++ app/javascript/controllers/chat_form_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["input"]
+
+    connect() {
+        console.log("ChatRoom controller connected")
+    }
+
+    handleKeyPress(event) {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault()
+            this.submit()
+        }
+    }
+
+    submit() {
+        if (this.inputTarget.value.trim() === "") {
+            return  // 입력이 비어있으면 제출하지 않음
+        }
+
+        const formData = new FormData(this.element)
+
+        fetch(this.element.action, {
+            method: this.element.method,
+            body: formData,
+            headers: {
+                "Accept": "text/vnd.turbo-stream.html",
+                "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content
+            }
+        })
+            .then(response => {
+                return response.text()
+            })
+            .then(html => {
+                Turbo.renderStreamMessage(html)
+                this.inputTarget.value = ""
+            })
+    }
+}
